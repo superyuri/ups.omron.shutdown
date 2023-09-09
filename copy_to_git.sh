d628a3c91a9c8181b659abd6c3127850ca67631a
EdwardYin <superyuri@yeah.net>
2023-09-09 12:15:11 +0900

    add bin files

---
+++ copy_to_git.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+mkdir -p "/tmp/GitHub/ups.omron.shutdown/source/ups.omron.shutdown/usr/local/emhttp/plugins/ups.omron.shutdown/"
+
+cp /usr/local/emhttp/plugins/ups.omron.shutdown/* /tmp/GitHub/ups.omron.shutdown/source/ups.omron.shutdown/usr/local/emhttp/plugins/ups.omron.shutdown -R -v -p
+
---
+++ pkg_build.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+cd source/ups.omron.shutdown
+chmod 0755 -R .
+./pkg_build.sh
